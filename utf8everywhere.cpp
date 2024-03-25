165189d2ab29ced8d6b8439f02c51cfc8c87b4f0
Andre Brand <andre.brand@haag-streit.com>
2024-03-25 07:21:23 +0100

    Switch to LLVM code formatting
    This reduces whitespaces and (hopefuly) works better when presenting.

--- utf8everywhere.cpp
+++ utf8everywhere.cpp
@@ -1,40 +1,51 @@
-#include "utils.h"
 #include <filesystem>
 #include <format>
 #include <fstream>
 #include <print>
 #include <string_view>
 
+#include "utils.h"
+
 #ifdef _WIN32
 #include <Windows.h>
 #endif
 
-int main()
-{
+int main() {
 #ifdef _WIN32
-	std::print("Active Code Page: {}\nOEM code page: {}\n", GetACP(), GetOEMCP());
+  std::print(
+      "Active code page: {}\nOEM code page: {}\nConsole output code page: {}\n",
+      GetACP(), GetOEMCP(), GetConsoleOutputCP());
 #endif
 
-	const std::filesystem::path OutputPath = std::filesystem::current_path() / "ω😆.txt";
-	std::fstream File(OutputPath, std::fstream::out | std::fstream::trunc);
-	std::print(File, "Wingdings are still a thing! 📫👓🖉☯\n");
-	std::print(File, "How do I spell my name now, Andr\u00E9, Andre\u0301, or Andre\u02CA?\n");
-	static_assert(std::string_view("é") == std::string_view("\u00E9"));
+  const std::filesystem::path OutputPath =
+      std::filesystem::current_path() / "ω😆.txt";
+  std::fstream File(OutputPath, std::fstream::out | std::fstream::trunc);
+  std::print(File, "Wingdings are still a thing! 📫👓🖉☯\n");
 
-	std::print("File created? {}\n", std::filesystem::exists(OutputPath) ? "💪" : "💩");
+  std::print("File created? {}\n",
+             std::filesystem::exists(OutputPath) ? "💪" : "💩");
 
-	const std::filesystem::path OutputPathUpperCase = std::filesystem::current_path() / "Ω😆.txt";
-	std::print("Greek is case-insensitive on Windows, too? {}\n",
-			   !std::filesystem::exists(OutputPathUpperCase) ? "✓" : "💔");
+  const std::filesystem::path OutputPathUpperCase =
+      std::filesystem::current_path() / "Ω😆.txt";
+  std::print("Greek is case-sensitive? {}\n",
+             !std::filesystem::exists(OutputPathUpperCase) ? "✓" : "💔");
+  std::print("Paths are {}equivalent! \n",
+             OutputPath == OutputPathUpperCase ? "" : "NOT ");
 
-	for (std::string_view View : {"µ", "a", "🐱", "\uFEFF"})
-	{
-		std::print("Code point {:#x} is rendered as {} and encoded with {} character(s) as {} or {}.\n",
-				   vu::getCodePoint(View.data()), View, View.size(), vu::formatAsHexadecimalValues(View),
-				   vu::formatAsBinaryValues(View));
-	}
+  for (std::string_view View : {"µ", "a", "🐱", "\uFEFF"}) {
+    std::print("Code point {:#x} is rendered as {} and encoded with {} "
+               "character(s) as {} or {}.\n",
+               vu::getCodePoint(View.data()), View, View.size(),
+               vu::formatAsHexadecimalValues(View),
+               vu::formatAsBinaryValues(View));
+  }
 
-	static_assert(std::string_view("\u00B5") == std::string_view("µ"));
-	static_assert(std::string_view("\u03BC") == std::string_view("μ"));
-	static_assert(std::string_view("µ") != std::string_view("μ"));
+  static_assert(std::string_view("\u00B5") == std::string_view("µ"));
+  static_assert(std::string_view("\u03BC") == std::string_view("μ"));
+  static_assert(std::string_view("µ") != std::string_view("μ"));
+
+  constexpr auto MoralDilemma = "How do I spell my name now, Andr\u00E9, Andre\u0301, or Andre\u02CA?\n";
+  std::print(File, MoralDilemma);
+  std::print(MoralDilemma);
+  static_assert(std::string_view("é") == std::string_view("\u00E9"));
 }
--- utils.h
+++ utils.h
@@ -5,101 +5,101 @@
 #include <format>
 #include <ranges>
 
-namespace vu
-{
+namespace vu {
 template <typename T>
 concept U8CompatibleChar = std::integral<T> && sizeof(T) == 1;
 
 template <typename T>
 concept U16CompatibleChar = std::integral<T> && sizeof(T) == 2;
 
-/// @note These methods would be trivial if the current implementation of std::format had all the fmt features,
-///	which is not yet the case in Visual Studio 17.9.4 even with C++23 enabled.
+/// @note These methods would be trivial if the current implementation of
+/// std::format had all the fmt features,
+///	which is not yet the case in Visual Studio 17.9.4 even with C++23
+/// enabled.
 
 /// @{
-auto formatAsHexadecimalValues(std::ranges::input_range auto&& frr_charArray)
-{
-    return std::ranges::fold_left(frr_charArray | std::views::transform([](U8CompatibleChar auto f_char) {
-                                      return std::format("\\{:#x}", std::bit_cast<std::uint8_t>(f_char));
-                                  }),
-                                  "", std::plus{});
+auto formatAsHexadecimalValues(std::ranges::input_range auto &&CharArray) {
+  return std::ranges::fold_left(
+      CharArray | std::views::transform([](U8CompatibleChar auto f_char) {
+        return std::format("\\{:#x}", std::bit_cast<std::uint8_t>(f_char));
+      }),
+      "", std::plus{});
 }
 
-auto formatAsBinaryValues(std::ranges::input_range auto&& frr_charArray)
-{
-    return std::ranges::fold_left_first(
-               frr_charArray | std::views::transform([](U8CompatibleChar auto f_char) {
-                   return std::format("{:0>8b}", std::bit_cast<std::uint8_t>(f_char));
-               }),
-               [](std::string f_left, std::string f_right) { return std::move(f_left) + " " + std::move(f_right); })
-        .value_or("");
+auto formatAsBinaryValues(std::ranges::input_range auto &&CharArray) {
+  return std::ranges::fold_left_first(
+             CharArray |
+                 std::views::transform([](U8CompatibleChar auto f_char) {
+                   return std::format("{:0>8b}",
+                                      std::bit_cast<std::uint8_t>(f_char));
+                 }),
+             [](std::string f_left, std::string f_right) {
+               return std::move(f_left) + " " + std::move(f_right);
+             })
+      .value_or("");
 }
 /// @}
 
 /// @brief implementation of Python's `ord` builtin.
-constexpr std::uint32_t getCodePoint(const U8CompatibleChar auto* fcp_utf8Encoded)
-{
-    const auto lc_firstByte = std::bit_cast<std::uint8_t>(*fcp_utf8Encoded);
-    const auto getNextByteValue = [](char f_char) -> std::uint32_t {
-        if ((std::bit_cast<std::uint8_t>(f_char) & 0b0100'0000u) != 0u)
-            throw std::runtime_error("invalid next byte");
-        return std::bit_cast<std::uint8_t>(f_char) & ~0b1000'0000u;
-    };
+constexpr std::uint32_t
+getCodePoint(const U8CompatibleChar auto *UTF8EncodedBytes) {
+  const auto lc_firstByte = std::bit_cast<std::uint8_t>(*UTF8EncodedBytes);
+  const auto getNextByteValue = [](char f_char) -> std::uint32_t {
+    if ((std::bit_cast<std::uint8_t>(f_char) & 0b0100'0000u) != 0u)
+      throw std::runtime_error("invalid next byte");
+    return std::bit_cast<std::uint8_t>(f_char) & ~0b1000'0000u;
+  };
 
-    const auto lc_numberOfBytes = std::countl_one(lc_firstByte);
-    if (lc_numberOfBytes == 0)
-        return lc_firstByte;
-    if (lc_numberOfBytes == 2)
-        return (static_cast<std::uint32_t>(lc_firstByte & ~0b1100'0000u) << 6) + getNextByteValue(fcp_utf8Encoded[1]);
-    if (lc_numberOfBytes == 3)
-        return (static_cast<std::uint32_t>(lc_firstByte & ~0b1110'0000u) << 12) +
-               (getNextByteValue(fcp_utf8Encoded[1]) << 6) + getNextByteValue(fcp_utf8Encoded[2]);
-    if (lc_numberOfBytes == 4)
-        return (static_cast<std::uint32_t>(lc_firstByte & ~0b1111'0000u) << 18) +
-               (getNextByteValue(fcp_utf8Encoded[1]) << 12) + (getNextByteValue(fcp_utf8Encoded[2]) << 6) +
-               getNextByteValue(fcp_utf8Encoded[3]);
+  const auto lc_numberOfBytes = std::countl_one(lc_firstByte);
+  if (lc_numberOfBytes == 0)
+    return lc_firstByte;
+  if (lc_numberOfBytes == 2)
+    return (static_cast<std::uint32_t>(lc_firstByte & ~0b1100'0000u) << 6) +
+           getNextByteValue(UTF8EncodedBytes[1]);
+  if (lc_numberOfBytes == 3)
+    return (static_cast<std::uint32_t>(lc_firstByte & ~0b1110'0000u) << 12) +
+           (getNextByteValue(UTF8EncodedBytes[1]) << 6) +
+           getNextByteValue(UTF8EncodedBytes[2]);
+  if (lc_numberOfBytes == 4)
+    return (static_cast<std::uint32_t>(lc_firstByte & ~0b1111'0000u) << 18) +
+           (getNextByteValue(UTF8EncodedBytes[1]) << 12) +
+           (getNextByteValue(UTF8EncodedBytes[2]) << 6) +
+           getNextByteValue(UTF8EncodedBytes[3]);
 
-    throw std::runtime_error("invalid start byte");
-}
-
-inline auto convertU8StringViewToRegularStringView(std::u8string_view f_u8StringView)
-{
-    return std::string_view{reinterpret_cast<const char*>(f_u8StringView.data()), f_u8StringView.size()};
+  throw std::runtime_error("invalid start byte");
 }
 
 template <U8CompatibleChar T_Char, typename T_Traits>
-auto printDefaultBOM(std::basic_ostream<T_Char, T_Traits>& fr_out) -> std::basic_ostream<T_Char, T_Traits>&
-{
-    return fr_out << "\xEF\xBB\xBF";
+auto printDefaultBOM(std::basic_ostream<T_Char, T_Traits> &OutputStream)
+    -> std::basic_ostream<T_Char, T_Traits> & {
+  return OutputStream << "\xEF\xBB\xBF";
 }
 
 template <U16CompatibleChar T_Char, typename T_Traits>
-auto printDefaultBOM(std::basic_ostream<T_Char, T_Traits>& fr_out) -> std::basic_ostream<T_Char, T_Traits>&
-{
-    // UTF-16 (LE) is the default encoding of the Windows Kernel
-    return fr_out << L"\xFFFE";
+auto printDefaultBOM(std::basic_ostream<T_Char, T_Traits> &OutputStream)
+    -> std::basic_ostream<T_Char, T_Traits> & {
+  // UTF-16 (LE) is the default encoding of the Windows Kernel
+  return OutputStream << L"\xFFFE";
 }
 
 // @return true iff the file has a Byte Order Mark (BOM)
-template <U8CompatibleChar T_Char, typename T_Traits> bool swallowBOM(std::basic_istream<T_Char, T_Traits>& fr_in)
-{
-    if (fr_in.peek() != 0xEF)
-        return false;
-    fr_in.get();
-    if (fr_in.peek() != 0xBB)
-    {
-        fr_in.unget();
-        return false;
-    }
-    fr_in.get();
-    if (fr_in.peek() != 0xBF)
-    {
-        fr_in.unget();
-        fr_in.unget();
-        return false;
-    }
-    fr_in.get();
-    return true;
+template <U8CompatibleChar T_Char, typename T_Traits>
+bool swallowBOM(std::basic_istream<T_Char, T_Traits> &InputStream) {
+  if (InputStream.peek() != 0xEF)
+    return false;
+  InputStream.get();
+  if (InputStream.peek() != 0xBB) {
+    InputStream.unget();
+    return false;
+  }
+  InputStream.get();
+  if (InputStream.peek() != 0xBF) {
+    InputStream.unget();
+    InputStream.unget();
+    return false;
+  }
+  InputStream.get();
+  return true;
 }
 
 } // namespace vu
